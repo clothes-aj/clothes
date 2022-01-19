d0b94f7fff14c6558915a3a2c9e59eff22a854f8
nayeon0731 <toothless1596@gmail.com>
2022-01-19 15:49:33 +0900

    location search

---
+++ loction_search.js
@@ -0,0 +1,26 @@
+
+const search = () => {
+    let location = document.querySelector('#location').value;
+    addrToXY(location);
+}
+
+const addrToXY = (location) => {
+    let key = 'f6d23cfd8deae9e2a6a4c287cf223bef';
+    let url = `https://dapi.kakao.com/v2/local/search/address.json?query=${location}`;
+    let lat, lon;
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": `KakaoAK ${key}`
+        }
+    }).then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+        return response.json();
+    }).then((data) => {
+        lat = data.documents[0].x;
+        lon = data.documents[0].y;
+        console.log(lat, lon);
+        app.fetchWeather(lat,lon);
+    });
+}
