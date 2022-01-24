ea4a8970e5a0b252e3b6e047087a834f0876f1ef
noion0511 <noion0511@gmail.com>
2022-01-24 19:46:20 +0900

    index 화면의 필요한 이미지 설정

--- image_search.js
+++ image_search.js
@@ -9,17 +9,17 @@ async function getUser() {
             'Authorization':'KakaoAK b62c85ae2923afa9548cf2e7734d7508'
         }
     };
-    let condition = 'coat'
 
-    fetch(`https://dapi.kakao.com/v2/search/image?query=${condition}&size=1`, config)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            document.getElementById("outer1").setAttribute("src", "https://search4.kakaocdn.net/argon/130x130_85_c/8dmwmkRM5Ga");
-            console.log(document.getElementsByClassName("recommendImage"));
-            document.getElementsByTagName()
-        })
-        .catch(error => console.log(error));
+    const arr = ['코트', '가죽자켓', '야상', '기모맨투맨', '니트', '기모후드티', '청바지', '슬랙스', '트레이닝바지'];
+    for (let i = 0; i < arr.length; i++) {
+        fetch(`https://dapi.kakao.com/v2/search/image?query=${arr[i]}&size=1`, config)
+            .then(response => response.json())
+            .then(response => {
+                // console.log(response)
+                document.getElementById(`clothes${i+1}`).setAttribute("src", response.documents[0].thumbnail_url);
+            })
+            .catch(error => console.log(error));
+    }
 }
 
 
